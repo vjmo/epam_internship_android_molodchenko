752355138eeeac665a71d5bb91d51cfeedbedd1e
vjmo <46070493+vjmo@users.noreply.github.com>
2021-06-16 18:18:41 +0300

    [Lection 10] [Molodchenko Veronika] Homework for lesson 10

--- lection_8/app/src/main/java/com/example/epam_internship_android_molodchenko/Meal.kt
+++ lection_8/app/src/main/java/com/example/epam_internship_android_molodchenko/Meal.kt
@@ -1,8 +1,12 @@
 package com.example.epam_internship_android_molodchenko
 
+import android.accessibilityservice.GestureDescription
+import java.io.FileDescriptor
+
 data class Meal (
     val id: Int,
     val name: String,
     val country: String,
     val category: String,
+    val description: String
 )
--- lection_8/app/src/main/java/com/example/epam_internship_android_molodchenko/MealListActivity.kt
+++ lection_8/app/src/main/java/com/example/epam_internship_android_molodchenko/MealListActivity.kt
@@ -1,27 +1,35 @@
 package com.example.epam_internship_android_molodchenko
 
+import android.content.Context
+import android.content.Intent
 import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.ListView
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MealListActivity : AppCompatActivity() {
 
-    var recyclerView: RecyclerView = RecyclerView(this)
-
-    val res: Resources = resources
-    val s1: Array<String> = arrayOf(arrayOf(R.array.meal_string).toString())
-    val images: Array<Int> = arrayOf(R.drawable.meal_one, R.drawable.meal_two)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meal_list)
 
-        recyclerView = findViewById(R.id.recyclerview)
+        val imageView: ImageView = findViewById(R.id.meal_one_img)
+        val textView: TextView = findViewById(R.id.txt_itm_meal_one)
 
-        var myAdapter: Adapter = Adapter(this, s1, images)
+        val intent = Intent(this, MealDetailsActivity::class.java)
+
+        imageView.setOnClickListener {
+            startActivity(intent)
+        }
+        textView.setOnClickListener {
+            startActivity(intent)
+        }
     }
+
 }
