73d71d9c4544ba22fadcb42169faa7d388015ad2
vjmo <46070493+vjmo@users.noreply.github.com>
2021-08-08 20:14:28 +0300

    Deleted

--- Example/app/src/main/java/com/example/example/Common.kt
+++
@@ -1,7 +0,0 @@
-package com.example.example
-
-object Common {
-    private val BASE_URL = "https://www.simplifiedcoding.net/demos/"
-    val retrofitService: RetrofitServices
-        get() = RetrofitClient.getClient(BASE_URL).create(RetrofitServices::class.java)
-}
