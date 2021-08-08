6ad92a36da24c772fff40028da3df69b22cb6468
vjmo <46070493+vjmo@users.noreply.github.com>
2021-08-08 20:14:28 +0300

    Deleted

--- Example/app/src/main/java/com/example/example/RetrofitServices.kt
+++
@@ -1,9 +0,0 @@
-package com.example.example
-
-import retrofit2.Call
-import retrofit2.http.*
-
-interface RetrofitServices {
-    @GET("marvel")
-    fun getMovieList(): Call<MutableList<Movie>>
-}
