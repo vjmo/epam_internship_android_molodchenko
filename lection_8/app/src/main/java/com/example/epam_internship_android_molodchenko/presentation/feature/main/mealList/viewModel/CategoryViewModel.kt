85c9eff51a971f787555f02dafa0195783bcda7a
vjmo <46070493+vjmo@users.noreply.github.com>
2021-09-25 17:07:40 +0300

    [Lection 17] [Molodchenko Veronika] Added second parameter

--- lection_8/app/src/main/java/com/example/epam_internship_android_molodchenko/presentation/feature/main/mealList/viewModel/CategoryViewModel.kt
+++ lection_8/app/src/main/java/com/example/epam_internship_android_molodchenko/presentation/feature/main/mealList/viewModel/CategoryViewModel.kt
@@ -2,6 +2,10 @@ package com.example.epam_internship_android_molodchenko.presentation.feature.mai
 
 import androidx.lifecycle.ViewModel
 import com.example.epam_internship_android_molodchenko.domain.useCase.GetCategoryUseCase
+import com.example.epam_internship_android_molodchenko.domain.useCase.RequestCategoryUseCase
 
-class CategoryViewModel(private val categoryUseCase: GetCategoryUseCase) : ViewModel() {
+class CategoryViewModel(
+    private val categoryUseCase: GetCategoryUseCase,
+    private val categoryRequestUseCase: RequestCategoryUseCase
+) : ViewModel() {
 }
--- lection_8/app/src/main/java/com/example/epam_internship_android_molodchenko/presentation/feature/main/mealList/viewModel/CategoryViewModelFactory.kt
+++ lection_8/app/src/main/java/com/example/epam_internship_android_molodchenko/presentation/feature/main/mealList/viewModel/CategoryViewModelFactory.kt
@@ -3,10 +3,14 @@ package com.example.epam_internship_android_molodchenko.presentation.feature.mai
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.epam_internship_android_molodchenko.domain.useCase.GetCategoryUseCase
+import com.example.epam_internship_android_molodchenko.domain.useCase.RequestCategoryUseCase
 
-class CategoryViewModelFactory(private val categoryUseCase: GetCategoryUseCase) :
+class CategoryViewModelFactory(
+    private val categoryUseCase: GetCategoryUseCase,
+    private val categoryRequestUseCase: RequestCategoryUseCase
+) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return CategoryViewModel(categoryUseCase) as T
+        return CategoryViewModel(categoryUseCase, categoryRequestUseCase) as T
     }
 }
