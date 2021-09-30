26c4c83fee83f85d412422269dc070f733b0f05f
vjmo <46070493+vjmo@users.noreply.github.com>
2021-09-30 11:57:53 +0300

    [Lection 17] [Molodchenko Veronika] Added ViewBinding (MealFilterFragment)

--- lection_8/app/src/main/java/com/example/epam_internship_android_molodchenko/presentation/feature/main/mealFilter/view/MealFilterFragment.kt
+++ lection_8/app/src/main/java/com/example/epam_internship_android_molodchenko/presentation/feature/main/mealFilter/view/MealFilterFragment.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import com.example.epam_internship_android_molodchenko.R
+import com.example.epam_internship_android_molodchenko.databinding.BottomSheetBinding
 import com.example.epam_internship_android_molodchenko.presentation.feature.main.mealFilter.view.clickListener.OnItemClickListenerFilter
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class MealFilterFragment : BottomSheetDialogFragment() {
 
-    //кликлист будет равняться налл
+    private lateinit var viewBinding: BottomSheetBinding
+/*
+    private val viewModel: MealViewModel by viewModels {
+        MealViewModelFactory(
+            GetMealListUseCase(
+                MealsRepositoryImpl(
+                    (RetrofitInstance.mealApi)
+                )
+            )
+        )
+    }*/
+
     var clickListener: OnItemClickListenerFilter? = null
 
     override fun onCreateView(
@@ -19,20 +29,18 @@ class MealFilterFragment : BottomSheetDialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.bottom_sheet, container, false)
+        viewBinding = BottomSheetBinding.inflate(inflater, container, false)
+        return viewBinding.root//inflater.inflate(R.layout.bottom_sheet, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //здесь эти 2 кнопки объявить
-        val buttonSortAsc = view.findViewById<Button>(R.id.asc_bttn)
-        val buttonSortDesc = view.findViewById<Button>(R.id.desc_bttn)
-        // здесь вызываю метод Кликлистнер по нажатию на кнопку , при этом сделать метод , который будет сеттить кликлистнер
-        buttonSortDesc.setOnClickListener {
+
+        viewBinding.descBttn.setOnClickListener {
             clickListener?.onItemClick(false)
             dismiss()
         }
-        buttonSortAsc.setOnClickListener {
+        viewBinding.ascBttn.setOnClickListener {
             clickListener?.onItemClick(true)
             dismiss()
         }
